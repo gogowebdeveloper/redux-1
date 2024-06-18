e36cd850827e6d4f1e3f1ea360feb8d4481b634b
Ganesh M Savadattimath <msganesh@digisprint.com>
2024-06-18 12:13:04 +0530

    updated auth with slice and rootReducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,27 @@
 import classes from './Counter.module.css';
 import { useDispatch, useSelector} from 'react-redux';
 import hideEye from "../assets/svg/Group.svg"
+import { counterActions } from './store';
 
 const Counter = () => {
   const dispatch=useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show=useSelector((state)=>state.showCounter)
+  const counter = useSelector((state) => state.counter.counter);
+  const show=useSelector((state)=>state.counter.showCounter);
 
   const updateIncrement=()=>{
-    dispatch({type:"increment"})
+    dispatch(counterActions.increment())
   }
   const updateDecrement=()=>{
-    dispatch({type:"decrement"})
+    dispatch(counterActions.decrement())
   }
-
+  const updateReset=()=>{
+    dispatch(counterActions.reset(0))  
+  }
   const handleUpdate=()=>{
-    dispatch({type:'increase',amount: 5})
+    dispatch(counterActions.increase(5))
   }
   const toggleCounterHandler = () => {
-    dispatch({type:"toggle"})
+    dispatch(counterActions.toggle())
   };
 
   return (
@@ -29,6 +32,7 @@ const Counter = () => {
       <button onClick={updateIncrement}>increment</button>
       <button onClick={handleUpdate}>increment + 5</button>
       <button onClick={updateDecrement}>decrement</button>
+      <button onClick={updateReset}>reset</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- src/components/store.js
+++ src/components/store.js
@@ -1,34 +1,83 @@
-import { createStore } from 'redux';
+import { createStore,combineReducers } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
-const initial={counter:0, showCounter: true}
-const countReducer = (state = initial, action) => {
-  switch (action.type) {
-    case 'increment':
-      return {
-        counter: state.counter + 1,
-        showCounter:state.showCounter
-      };
-    case 'decrement':
-      return {
-        counter: state.counter - 1,
-        showCounter:state.showCounter
-      };
-      case "increase":
-        return{
-            counter:state.counter + action.amount,
-            showCounter:state.showCounter
+const counterInitialState={counter:0,showCounter:true}
+const authInitialState={isAuthenticated:false}
 
-        };
-        case "toggle":
-            return{
-                showCounter: !state.showCounter,
-                counter:state.counter
-            }
-    default:
-      return state;
+const counterSlice=createSlice({
+  name:"counter",
+  initialState:counterInitialState,
+  reducers:{
+    increment(state){
+      state.counter++
+    },
+    decrement(state){
+      state.counter--
+    },
+    reset(state,action){
+      state.counter=state.counter*action.payload
+    },
+    increase(state,action){
+      state.counter=state.counter+action.payload
+    },
+    toggle(state){
+      state.showCounter=!state.showCounter
+    }
   }
-};
+})
 
-const store = createStore(countReducer);
+const authSlice=createSlice({
+  name:"authentication",
+  initialState:authInitialState,
+  reducer:{
+    login(state){
+      state.isAuthenticated=true
+    },
+    logout(state){
+      state.isAuthenticated=false
+    },
+  }
+})
+// const initial={counter:0, showCounter: true}
+// const countReducer = (state = initial, action) => {
+//   switch (action.type) {
+//     case 'increment':
+//       return {
+//         counter: state.counter + 1,
+//         showCounter:state.showCounter
+//       };
+//     case 'decrement':
+//       return {
+//         counter: state.counter - 1,
+//         showCounter:state.showCounter
+//       };
+//       case "reset":
+//       return{
+//         counter:state.counter*0,
+//         showCounter:state.showCounter
+//       };
+//       case "increase":
+//         return{
+//             counter:state.counter + action.amount,
+//             showCounter:state.showCounter
+
+//         };
+//         case "toggle":
+//             return{
+//                 showCounter: !state.showCounter,
+//                 counter:state.counter
+//             }
+//     default:
+//       return state;
+//   }
+// };
+export const counterActions=counterSlice.actions;
+export const authActions=authSlice.actions;
+const rootReducer=combineReducers({
+  counter:counterSlice.reducer,
+  auth:authSlice.reducer
+})
+
+const store = createStore(rootReducer);
 
 export default store;
